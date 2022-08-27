9cc854669c8b87a11fddfcdaea7ee0763b0991cd
0xperp <0xperp@protonmail.com>
2022-08-27 11:16:51 -0400

    feat: initial utps

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import asyncio
 import logging
 from marginpy.logger import setup_logging
 from marginpy import MarginfiClient
+from marginpy.types import (
+    MarginRequirement,
+)
 import os
 from os.path import join, dirname
 
@@ -32,15 +35,14 @@ async def main():
 class PrometheusClient:
     def __init__(self):
         self.metrics = "http://localhost:" + str(prometheus_endpoint)
-        # self.g = Gauge("marginfi_account", 'Marginfi Account Information', ['marginfi_address', 'borrows', 'balances', 'margin_requirement', 'deposits', 'active_utps'])
-        # self.g = Gauge("mango_utp_account", 'Mango UTP Account Information', ['marginfi_address', 'mango_address', 'equity', 'free_collateral', 'liquidation_value'])
-        # self.g = Gauge("zo_utp_account", 'Zo UTP Account Information', ['marginfi_address', 'zo_address', 'equity', 'free_collateral', 'liquidation_value'])
 
         # marginfi gauges
         self.marginfi_total_accounts = Gauge("marginfi_total_accounts", "Marginfi Total Accounts Created", ["marginfi"])
         self.marginfi_borrows = Gauge("marginfi_account_borrows", 'Marginfi Account Borrows', ['marginfi_address'])
         self.marginfi_deposits = Gauge("marginfi_account_deposits", 'Marginfi Account Deposits', ['marginfi_address'])
-        # self.marginfi_margin_requirement = Gauge("marginfi_account_margin_requirement", 'Marginfi Account Margin Requirement', ['marginfi_address'])
+        self.marginfi_margin_requirement_initial = Gauge("marginfi_margin_requirement_initial", 'Marginfi Account Margin Initial Requirement', ['marginfi_address'])
+        self.marginfi_margin_requirement_maintenance = Gauge("marginfi_margin_requirement_maintenance", 'Marginfi Account Margin Maintenance Requirement', ['marginfi_address'])
+        self.marginfi_margin_requirement_partial_liquidation = Gauge("marginfi_margin_requirement_partial_liquidation", 'Marginfi Account Margin Partial Liquidation Requirement', ['marginfi_address'])
         self.marginfi_equity = Gauge("marginfi_account_equity", 'Marginfi Account Equity', ['marginfi_address'])
         self.marginfi_assets = Gauge("marginfi_account_assets", 'Marginfi Account Assets', ['marginfi_address'])
         self.marginfi_liabilities = Gauge("marginfi_account_liabilities", 'Marginfi Account Liabilities', ['marginfi_address'])
@@ -57,8 +59,6 @@ class PrometheusClient:
         
     async def marginfi(self):
         client = await MarginfiClient.from_env()
-
-        # TODO: Load _all_ accounts here
         all_accounts = await client.load_all_marginfi_account_addresses()
 
         # setting total accounts gauge
@@ -68,18 +68,16 @@ class PrometheusClient:
         for account in all_accounts:
             account = await client.load_marginfi_account(account)
 
-            # await account.observe_utps()
-            # marginfi
-            active_utps = account.active_utps # can grab all active accounts here?
-
+            # marginfi information
             authority = account.authority
             borrow = account.borrows
             balances = account.compute_balances()
             equity = balances.equity
             assets = balances.assets
             liabilities = balances.liabilities
-            # TODO: compute margins across the board
-            # margin_requirement = account.compute_margin_requirement(MarginRequir) # for types of margin to check for see here https://mrgnlabs.github.io/marginfi-sdk/marginpy/html/marginpy.types.html#marginpy.types.MarginRequirement
+            margin_requirement_initial = account.compute_margin_requirement(MarginRequirement.INITIAL)
+            margin_requirement_maintenance = account.compute_margin_requirement(MarginRequirement.MAINTENANCE)
+            margin_requirement_partial_liquidation = account.compute_margin_requirement(MarginRequirement.PARTIAL_LIQUIDATION)
             deposits = account.deposits
 
             # mango
@@ -94,25 +92,27 @@ class PrometheusClient:
             zo_free_collateral = account.zo.free_collateral
             zo_liquidation_value = account.zo.liquidation_value
 
-            # Setting marginfi gauges
+            # setting marginfi gauges
             self.marginfi_borrows.labels(marginfi_address=authority).set(borrow)
             self.marginfi_deposits.labels(marginfi_address=authority).set(deposits)
-            # self.marginfi_margin_requirement.labels(marginfi_address=authority).set(margin_requirement)
+            self.marginfi_margin_requirement_initial.labels(marginfi_address=authority).set(margin_requirement_initial)
+            self.marginfi_margin_requirement_maintenance.labels(marginfi_address=authority).set(margin_requirement_maintenance)
+            self.marginfi_margin_requirement_partial_liquidation.labels(marginfi_address=authority).set(margin_requirement_partial_liquidation)
             self.marginfi_equity.labels(marginfi_address=authority).set(equity)
             self.marginfi_assets.labels(marginfi_address=authority).set(assets)
             self.marginfi_liabilities.labels(marginfi_address=authority).set(liabilities)
 
-            # Settings mango gauges
+            # settings mango gauges
             self.mango_equity.labels(mango_address=mango_address).set(mango_equity)
             self.mango_free_collateral.labels(mango_address=mango_address).set(mango_free_collateral)
             self.mango_liquidation_value.labels(mango_address=mango_address).set(mango_liquidation_value)
 
-            # Settings mango gauges
+            # settings mango gauges
             self.zo_equity.labels(zo_address=zo_address).set(zo_equity)
             self.zo_free_collateral.labels(zo_address=zo_address).set(zo_free_collateral)
             self.zo_liquidation_value.labels(zo_address=zo_address).set(zo_liquidation_value)
     
-    # TODO: View funding, open orders, etc
+    # TODO: View funding, open orders, etc. for UTPs
 
 if __name__ == '__main__':
     asyncio.run(main())
