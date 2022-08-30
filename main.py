78247071a100b93961c1458042cbb9f9c3c98b49
Nate <96147421+0xperp@users.noreply.github.com>
2022-08-29 21:44:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -37,25 +37,25 @@ class PrometheusClient:
         self.metrics = "http://localhost:" + str(prometheus_endpoint)
 
         # marginfi gauges
-        self.marginfi_total_accounts = Gauge("marginfi_total_accounts", "Marginfi Total Accounts Created", ["marginfi"])
-        self.marginfi_borrows = Gauge("marginfi_account_borrows", 'Marginfi Account Borrows', ['marginfi_address'])
-        self.marginfi_deposits = Gauge("marginfi_account_deposits", 'Marginfi Account Deposits', ['marginfi_address'])
-        self.marginfi_margin_requirement_initial = Gauge("marginfi_margin_requirement_initial", 'Marginfi Account Margin Initial Requirement', ['marginfi_address'])
-        self.marginfi_margin_requirement_maintenance = Gauge("marginfi_margin_requirement_maintenance", 'Marginfi Account Margin Maintenance Requirement', ['marginfi_address'])
-        self.marginfi_margin_requirement_partial_liquidation = Gauge("marginfi_margin_requirement_partial_liquidation", 'Marginfi Account Margin Partial Liquidation Requirement', ['marginfi_address'])
-        self.marginfi_equity = Gauge("marginfi_account_equity", 'Marginfi Account Equity', ['marginfi_address'])
-        self.marginfi_assets = Gauge("marginfi_account_assets", 'Marginfi Account Assets', ['marginfi_address'])
-        self.marginfi_liabilities = Gauge("marginfi_account_liabilities", 'Marginfi Account Liabilities', ['marginfi_address'])
+        self.marginfi_total_accounts = Gauge("marginfi_total_accounts", "Marginfi Total Accounts Created", ['marginfi'])
+        self.marginfi_borrows = Gauge("marginfi_account_borrows", 'Marginfi Account Borrows', ['marginfi_address', 'mango_address', 'zo_address'])
+        self.marginfi_deposits = Gauge("marginfi_account_deposits", 'Marginfi Account Deposits', ['marginfi_address', 'mango_address', 'zo_address'])
+        self.marginfi_margin_requirement_initial = Gauge("marginfi_margin_requirement_initial", 'Marginfi Account Margin Initial Requirement', ['marginfi_address', 'mango_address', 'zo_address'])
+        self.marginfi_margin_requirement_maintenance = Gauge("marginfi_margin_requirement_maintenance", 'Marginfi Account Margin Maintenance Requirement', ['marginfi_address', 'mango_address', 'zo_address'])
+        self.marginfi_margin_requirement_partial_liquidation = Gauge("marginfi_margin_requirement_partial_liquidation", 'Marginfi Account Margin Partial Liquidation Requirement', ['marginfi_address', 'mango_address', 'zo_address'])
+        self.marginfi_equity = Gauge("marginfi_account_equity", 'Marginfi Account Equity', ['marginfi_address', 'mango_address', 'zo_address'])
+        self.marginfi_assets = Gauge("marginfi_account_assets", 'Marginfi Account Assets', ['marginfi_address', 'mango_address', 'zo_address'])
+        self.marginfi_liabilities = Gauge("marginfi_account_liabilities", 'Marginfi Account Liabilities', ['marginfi_address', 'mango_address', 'zo_address'])
 
         # mango gauges
-        self.mango_equity = Gauge("mango_account_equity", 'Mango Account Equity', ['mango_address'])
-        self.mango_free_collateral = Gauge("mango_account_free_collateral", 'Mango Account Free Collateral', ['mango_address'])
-        self.mango_liquidation_value = Gauge("mango_account_liquidation_value", 'Mango Account Liquidation Value', ['mango_address'])
+        self.mango_equity = Gauge("mango_account_equity", 'Mango Account Equity', ['marginfi_address', 'mango_address', 'zo_address'])
+        self.mango_free_collateral = Gauge("mango_account_free_collateral", 'Mango Account Free Collateral', ['marginfi_address', 'mango_address', 'zo_address'])
+        self.mango_liquidation_value = Gauge("mango_account_liquidation_value", 'Mango Account Liquidation Value', ['marginfi_address', 'mango_address', 'zo_address'])
 
         # zo gauges
-        self.zo_equity = Gauge("zo_account_equity", 'Zo Account Equity', ['zo_address'])
-        self.zo_free_collateral = Gauge("zo_account_free_collateral", 'Zo Account Free Collateral', ['zo_address'])
-        self.zo_liquidation_value = Gauge("zo_account_liquidation_value", 'Zo Account Liquidation Value', ['zo_address'])
+        self.zo_equity = Gauge("zo_account_equity", 'Zo Account Equity', ['marginfi_address', 'mango_address', 'zo_address'])
+        self.zo_free_collateral = Gauge("zo_account_free_collateral", 'Zo Account Free Collateral', ['marginfi_address', 'mango_address', 'zo_address'])
+        self.zo_liquidation_value = Gauge("zo_account_liquidation_value", 'Zo Account Liquidation Value', ['marginfi_address', 'mango_address', 'zo_address'])
         
     async def marginfi(self):
         client = await MarginfiClient.from_env()
@@ -93,24 +93,24 @@ class PrometheusClient:
             zo_liquidation_value = account.zo.liquidation_value
 
             # setting marginfi gauges
-            self.marginfi_borrows.labels(marginfi_address=authority).set(borrow)
-            self.marginfi_deposits.labels(marginfi_address=authority).set(deposits)
-            self.marginfi_margin_requirement_initial.labels(marginfi_address=authority).set(margin_requirement_initial)
-            self.marginfi_margin_requirement_maintenance.labels(marginfi_address=authority).set(margin_requirement_maintenance)
-            self.marginfi_margin_requirement_partial_liquidation.labels(marginfi_address=authority).set(margin_requirement_partial_liquidation)
-            self.marginfi_equity.labels(marginfi_address=authority).set(equity)
-            self.marginfi_assets.labels(marginfi_address=authority).set(assets)
-            self.marginfi_liabilities.labels(marginfi_address=authority).set(liabilities)
+            self.marginfi_borrows.labels(marginfi_address=authority, mango_address=mango_address, zo_address=zo_address).set(borrow)
+            self.marginfi_deposits.labels(marginfi_address=authority, mango_address=mango_address, zo_address=zo_address).set(deposits)
+            self.marginfi_margin_requirement_initial.labels(marginfi_address=authority, mango_address=mango_address, zo_address=zo_address).set(margin_requirement_initial)
+            self.marginfi_margin_requirement_maintenance.labels(marginfi_address=authority, mango_address=mango_address, zo_address=zo_address).set(margin_requirement_maintenance)
+            self.marginfi_margin_requirement_partial_liquidation.labels(marginfi_address=authority, mango_address=mango_address, zo_address=zo_address).set(margin_requirement_partial_liquidation)
+            self.marginfi_equity.labels(marginfi_address=authority, mango_address=mango_address, zo_address=zo_address).set(equity)
+            self.marginfi_assets.labels(marginfi_address=authority, mango_address=mango_address, zo_address=zo_address).set(assets)
+            self.marginfi_liabilities.labels(marginfi_address=authority, mango_address=mango_address, zo_address=zo_address).set(liabilities)
 
             # settings mango gauges
-            self.mango_equity.labels(mango_address=mango_address).set(mango_equity)
-            self.mango_free_collateral.labels(mango_address=mango_address).set(mango_free_collateral)
-            self.mango_liquidation_value.labels(mango_address=mango_address).set(mango_liquidation_value)
+            self.mango_equity.labels(marginfi_address=authority, mango_address=mango_address, zo_address=zo_address).set(mango_equity)
+            self.mango_free_collateral.labels(marginfi_address=authority, mango_address=mango_address, zo_address=zo_address).set(mango_free_collateral)
+            self.mango_liquidation_value.labels(marginfi_address=authority, mango_address=mango_address, zo_address=zo_address).set(mango_liquidation_value)
 
             # settings mango gauges
-            self.zo_equity.labels(zo_address=zo_address).set(zo_equity)
-            self.zo_free_collateral.labels(zo_address=zo_address).set(zo_free_collateral)
-            self.zo_liquidation_value.labels(zo_address=zo_address).set(zo_liquidation_value)
+            self.zo_equity.labels(marginfi_address=authority, mango_address=mango_address, zo_address=zo_address).set(zo_equity)
+            self.zo_free_collateral.labels(marginfi_address=authority, mango_address=mango_address, zo_address=zo_address).set(zo_free_collateral)
+            self.zo_liquidation_value.labels(marginfi_address=authority, mango_address=mango_address, zo_address=zo_address).set(zo_liquidation_value)
     
     # TODO: View funding, open orders, etc. for UTPs
 
